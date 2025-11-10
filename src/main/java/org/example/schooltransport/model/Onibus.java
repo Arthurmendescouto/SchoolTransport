57885561f3c01abb226e442b1e3cd6c7ff8e2915
filipeasj <202421190039@ifba.edu.br>
2025-11-10 11:16:00 -0300

    feat: adicionadas classes dentro de'model'

---
+++ src/main/java/org/example/schooltransport/model/Onibus.java
@@ -0,0 +1,20 @@
+package org.example.schooltransport.model;
+
+public class Onibus {
+    String id;
+
+    public Onibus(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    
+    
+}
