4117ab186766869dc6aa6e2ad1a783460fe43668
FILIPE ALVES <filipealves.info@gmail.com>
2025-11-10 17:04:35 -0300

    feat: ajustes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,9 @@ module org.example.schooltransport {
     requires javafx.base;
     requires javafx.graphics;
 
-
     opens org.example.schooltransport to javafx.fxml;
+    opens org.example.schooltransport.controllers to javafx.fxml;
+
     exports org.example.schooltransport;
+    exports org.example.schooltransport.controllers;
 }
